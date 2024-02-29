467bb7361586b7f416a75dba155f88d0d8ae8e15
Maria Kovatchka <maria.kovatchka@kellogg.northwestern.edu>
2024-02-29 22:50:37 +0000

    Some progress

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,15 +1,21 @@
 class EntriesController < ApplicationController
 
   def new
+    @user = User.find_by({ "id" => session["user_id"] })
   end
 
   def create
+    @user = User.find_by({ "id" => session["user_id"] })
+  if @user != nil
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
     @entry.save
+  else
+    flash["notice"] = "Login first."
+  end
     redirect_to "/places/#{@entry["place_id"]}"
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema[7.1].define(version: 2022_03_09_020701) do
     t.string "title"
     t.text "description"
     t.date "occurred_on"
-    t.string "image"
     t.integer "place_id"
     t.integer "user_id"
     t.datetime "created_at", null: false

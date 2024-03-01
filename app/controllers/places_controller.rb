70426e6bf640fb6e5582485839b12f341bf2ccb9
Maria Kovatchka <maria.kovatchka@kellogg.northwestern.edu>
2024-03-01 16:19:50 +0000

    Continued

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -15,8 +15,17 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new
     @place["name"] = params["name"]
-    @place.save
-    redirect_to "/places"
-  end
+    existing_place = Place.find_by("name" => @place["name"])
+    if existing_place
+      flash[:notice] = "A place with the name #{@place["name"]} already exists."
+      redirect_to "/places/new"
 
+    elsif @place["name"].present?
+    @place.save
+      redirect_to "/places"
+    else 
+    flash[:notice] = "Please enter the name of a new place!"
+    redirect_to "/places/new"
+    end
+  end
 end

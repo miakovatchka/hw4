802955e7d53dafe4b940b23ea0b1fa3ec931c712
Maria Kovatchka <maria.kovatchka@kellogg.northwestern.edu>
2024-03-01 16:48:52 +0000

    Bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    if User.find_by({ "username" => params["username"] }) == nil
-      @user = User.new
-      @user["username"] = params["username"]
-      @user["password"] = BCrypt::Password.create(params["password"])
-      @user.save
-      redirect_to "/login"
+    existing_user_by_email = User.find_by("email" => params["email"])
+    if existing_user_by_email
+    flash["notice"] = "This email already exists in our database."
+    redirect_to "/users/new"
+  else
+    @user = User.new
+    @user["username"] = params["username"]
+    @user["email"] = params["email"]
+    @user["password"] = BCrypt::Password.create(params["password"])
+    if @user.save
+    redirect_to "/login"
     else
-      flash["notice"] = "Username taken."
+      flash["notice"] = "There was an issue creating your account. Please try again later."
       redirect_to "/users/new"
-    end
   end
+end
+end
 end
